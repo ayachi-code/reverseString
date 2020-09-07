af079cd4f93844986053722eed5a4e9a5a2739cc
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-09-07 16:43:36 +0200

    cstring geimporteerd

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
 string reverseString(string word) {
+
     return "test";
 }
 
 int main() {
+    reverseString("Hallo");
     return 0;
 }
