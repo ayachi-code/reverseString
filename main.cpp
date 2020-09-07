8c8ac40e54666d03df5d8917d53f3f6c7501b1ce
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-09-07 16:49:32 +0200

    laatste ellemnt [index] van het woord word opgeslagen in variable [int]

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 string reverseString(string word) {
     char c_reverseString[word.size() + 1];
     strcpy(c_reverseString,word.c_str());
+    int lastEllementReverseString = sizeof(c_reverseString) - 2;
+    cout << c_reverseString[lastEllementReverseString] << endl;
     return "test";
 }
 
